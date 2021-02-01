1d9e83ba79399c154975f680c54903375f7835cb
lizhangray <37239596+lizhangray@users.noreply.github.com>
2021-02-01 21:03:10 +0800

    Update run_bak.py

--- run_bak.py
+++ run_bak.py
@@ -1,8 +1,8 @@
-# Authorized by Li Zhan: lizhangray@qq.com on the date 20210201
 # Reference Paper: 
 #   Z. Li, J. Zhang, R. Zhong, B. Bhanu, Y. Chen, Q. Zhang, H. Tang
 #   Lightweight and Efficient Image Dehazing Network Guided by Transmission Estimation from Real-world Hazy Scenes
 #   Sensors. MDPI, Basel, Switzerland. 1 February 2021 
+# Any questions please contact：lizhangray@qq.com
 
 
 import tensorflow as tf
